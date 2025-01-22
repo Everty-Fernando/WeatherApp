9e89842d1e67dd88507ca0f1ffdcc096e53b3962
Everty-Fernando <75959602+Everty-Fernando@users.noreply.github.com>
2025-01-22 17:38:01 -0300

    feat: creating buildSrc to manage dependencies

---
+++ buildSrc/src/main/kotlin/FeatureModules.kt
@@ -0,0 +1,9 @@
+object FeatureModules {
+
+    const val app = ":app"
+
+    //Home
+    const val homeUI = ":features:home:home_ui"
+    const val homeDomain = ":features:home:home_domain"
+    const val homeData = ":features:home:home_data"
+}
---
+++ buildSrc/src/main/kotlin/SharedModules.kt
@@ -0,0 +1,5 @@
+object SharedModules {
+    const val network = ":shared:network"
+    const val utils = ":shared:utils"
+    const val presentation = ":shared:presentation"
+}
